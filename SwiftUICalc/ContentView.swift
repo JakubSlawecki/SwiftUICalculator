bcfda3ccd36e619d3c8a3521c46d28fa52ce3e8f
Jakub Slawecki <jakub.slawecki@hemmersbach.com>
2020-01-13 09:46:01 +0100

    Basic UI

--- SwiftUICalc/ContentView.swift
+++ SwiftUICalc/ContentView.swift
@@ -9,8 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ForEach(buttons, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { button in
+                        Text(button)
+                        .font(.system(size: 32))
+                            .frame(width: 80, height: 80)
+                        
+                    }
+                }
+            }
+        }
     }
 }
 

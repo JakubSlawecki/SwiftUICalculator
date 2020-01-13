76b05697bc9096e6c7b009d791f56b9efe00abf8
Jakub Slawecki <jakub.slawecki@hemmersbach.com>
2020-01-13 09:10:20 +0100

    starter project

---
+++ SwiftUICalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalc
+//
+//  Created by Jakub Slawecki on 13/01/2020.
+//  Copyright © 2020 Jakub Slawecki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

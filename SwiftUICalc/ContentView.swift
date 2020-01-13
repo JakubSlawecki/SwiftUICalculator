bc6276654fe9ffffd9f11c0f201459e79fb37196
Jakub Slawecki <jakub.slawecki@hemmersbach.com>
2020-01-13 16:33:45 +0100

    Added Enum for button titles etc

--- SwiftUICalc/ContentView.swift
+++ SwiftUICalc/ContentView.swift
@@ -8,13 +8,62 @@
 
 import SwiftUI
 
+enum CalculatorButton: String {
+    
+    case zero, one, two, three, four, five, six, seven, eight, nine, dot
+    case equals, plus, minus, multiply, divide
+    case decimal
+    case ac, plusMinus, percentage
+    
+    var title: String {
+        
+        switch self {
+        case .zero:       return "0"
+        case .one:        return "1"
+        case .two:        return "2"
+        case .three:      return "3"
+        case .four:       return "4"
+        case .five:       return "5"
+        case .six:        return "6"
+        case .seven:      return "7"
+        case .eight:      return "8"
+        case .nine:       return "9"
+        case .plusMinus:  return "-/+"
+        case .percentage: return "%"
+        case .divide:     return "/"
+        case .multiply:   return "X"
+        case .minus:      return "-"
+        case .plus:       return "+"
+        case .equals:     return "="
+        case .decimal:    return "."
+            
+        default:
+            return "AC"
+        }
+    }
+    
+    var background: Color {
+        switch self {
+        case .zero, .one, .two, . three, .four, .five, .six, .seven, .eight, .nine, .decimal:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percentage:
+            return Color(.lightGray)
+        default:
+            return Color.orange
+        }
+    }
+    
+}
+
 struct ContentView: View {
     
-    let buttons = [
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", ".", "="]
+    let buttons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percentage, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .decimal, .equals]
+        
     ]
     
     var body: some View {
@@ -34,12 +83,19 @@ struct ContentView: View {
                 ForEach(buttons, id: \.self) { row in
                     HStack (spacing: 12) {
                         ForEach(row, id: \.self) { button in
-                            Text(button)
-                            .font(.system(size: 32))
-                                .frame(width: self.getButtonWidth(), height: self.getButtonWidth())
-                                .foregroundColor(.white)
-                                .background(Color.yellow)
-                                .cornerRadius(self.getButtonWidth())
+                            
+                            Button(action: {
+                                
+                            }) {
+                                Text(button.title)
+                                .font(.system(size: 32))
+                                    .frame(width: self.getButtonWidth(button: button), height: self.getButtonHeight())
+                                    .foregroundColor(.white)
+                                    .background(button.background)
+                                    .cornerRadius(self.getButtonWidth(button: button))
+                            }
+                            
+                            
                             
                         }
                     }
@@ -52,7 +108,14 @@ struct ContentView: View {
     
     
     
-    func getButtonWidth() -> CGFloat {
+    func getButtonWidth(button: CalculatorButton) -> CGFloat {
+        if button == .zero {
+            return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
+        }
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
+    }
+    
+    func getButtonHeight() -> CGFloat {
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
